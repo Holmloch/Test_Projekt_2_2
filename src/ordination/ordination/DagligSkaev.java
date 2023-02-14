b3d34b6584c8c0806e545857c2f8fdbb02c77019
sodiee <mathi.carlsen@gmail.com>
2023-02-14 09:48:21 +0100

    ordination

--- src/ordination/ordination/DagligSkaev.java
+++ src/ordination/ordination/DagligSkaev.java
@@ -1,12 +1,32 @@
 package ordination.ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination{
+    private LocalDate startDen;
+    private LocalDate slutDen;
+    private ArrayList<Dosis> dosis;
 
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+    }
+
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
