b40dedac2b6c41cd72055e66eeb9b66a976c739a
elias <eliasasmark@hotmail.com>
2023-02-16 11:33:02 +0100

    t14_opretDosis

--- test/ordination/ordination/DagligSkaevTest.java
+++ test/ordination/ordination/DagligSkaevTest.java
@@ -11,13 +11,11 @@ class DagligSkaevTest {
 
     @Test
     void t14_opretDosis() {
-        //arrange
+
         Patient patient = new Patient("59841720", "Bingo Dorte", 78);
         LocalTime[] kl = { LocalTime.of(12, 0), LocalTime.of(12, 40), LocalTime.of(16, 0), LocalTime.of(18, 45) };
         double[] an = { 0.5, 1, 2.5, 3 };
 
-        //act
-        //Assert
         DagligSkaev dagligSkaev = new DagligSkaev(LocalDate.of(2022, 2, 3), LocalDate.of(2022, 2, 9), patient, kl, an);
         Dosis dosis = dagligSkaev.opretDosis(LocalTime.of(10, 30), 4);
 
