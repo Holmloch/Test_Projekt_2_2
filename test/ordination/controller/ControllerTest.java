0c72fa5abefcb6efaeefef026f3cf56d047a69c4
holml <holmloch@gmail.com>
2023-02-16 09:00:04 +0100

    yo

---
+++ test/ordination/controller/ControllerTest.java
@@ -0,0 +1,14 @@
+package ordination.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void opretDagligFastOrdination() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn() {
+    }
+}
