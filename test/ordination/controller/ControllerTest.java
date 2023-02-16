394d34e19c7af6e9d65cf65d2551e5c2286a2623
holml <holmloch@gmail.com>
2023-02-16 09:56:25 +0100

    yo

--- test/ordination/controller/ControllerTest.java
+++ test/ordination/controller/ControllerTest.java
@@ -33,4 +33,56 @@ class ControllerTest {
         double forventetAnbefaletDosisPrDoegn = 13.0;
         assertEquals(forventetAnbefaletDosisPrDoegn,faktiskAnbefaletDosisPrDoegn);
     }
+
+    @Test
+    void tc2_anbefaletDosisPrDoegn() {
+        //Arrange
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        Patient patient = new Patient("123234-4937", "Børge Børgesen", 24);
+        //Act
+        double faktiskAnbefaletDosisPrDoegn = Controller.getTestController().anbefaletDosisPrDoegn(patient, laegemiddel);
+
+        //Assert
+        double forventetAnbefaletDosisPrDoegn = 24.0;
+        assertEquals(forventetAnbefaletDosisPrDoegn,faktiskAnbefaletDosisPrDoegn);
+    }
+
+    @Test
+    void tc3_anbefaletDosisPrDoegn() {
+        //Arrange
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        Patient patient = new Patient("123234-4937", "Jane Jensen", 63.4);
+        //Act
+        double faktiskAnbefaletDosisPrDoegn = Controller.getTestController().anbefaletDosisPrDoegn(patient, laegemiddel);
+
+        //Assert
+        double forventetAnbefaletDosisPrDoegn = 95.1;
+        assertEquals(forventetAnbefaletDosisPrDoegn,faktiskAnbefaletDosisPrDoegn);
+    }
+
+    @Test
+    void tc4_anbefaletDosisPrDoegn() {
+        //Arrange
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        Patient patient = new Patient("123234-4937", "Børge Børgesen", 120);
+        //Act
+        double faktiskAnbefaletDosisPrDoegn = Controller.getTestController().anbefaletDosisPrDoegn(patient, laegemiddel);
+
+        //Assert
+        double forventetAnbefaletDosisPrDoegn = 180.0;
+        assertEquals(forventetAnbefaletDosisPrDoegn,faktiskAnbefaletDosisPrDoegn);
+    }
+
+    @Test
+    void tc5_anbefaletDosisPrDoegn() {
+        //Arrange
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        Patient patient = new Patient("123234-4937", "Børge Børgesen", 126);
+        //Act
+        double faktiskAnbefaletDosisPrDoegn = Controller.getTestController().anbefaletDosisPrDoegn(patient, laegemiddel);
+
+        //Assert
+        double forventetAnbefaletDosisPrDoegn = 252.0;
+        assertEquals(forventetAnbefaletDosisPrDoegn,faktiskAnbefaletDosisPrDoegn);
+    }
 }
