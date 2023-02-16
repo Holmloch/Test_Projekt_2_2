f76651b7e2679469811face92fc7c6acf5f831b8
elias <eliasasmark@hotmail.com>
2023-02-16 09:53:58 +0100

    tc8_doegnDosis

--- test/ordination/ordination/DagligFastTest.java
+++ test/ordination/ordination/DagligFastTest.java
@@ -2,11 +2,21 @@ package ordination.ordination;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligFastTest {
 
     @Test
-    void doegnDosis() {
+    void tc8_doegnDosis() {
+        //arrange
+        Patient patient = new Patient("1235679", "John Dillermand", 80);
+        DagligFast dagligFast = new DagligFast(LocalDate.of(2012, 12, 12), LocalDate.of(2012, 12, 12), 2, 0, 1, 0, patient);
+        //act
+        double faktiskDaglig = dagligFast.doegnDosis();
+        //assert
+        double forventetDaglig = 3.0;
+        assertEquals(forventetDaglig, faktiskDaglig);
     }
 }
