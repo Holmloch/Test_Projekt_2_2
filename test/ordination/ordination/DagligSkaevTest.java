d667f2ba83af610ebba9184161e59253d9061985
elias <eliasasmark@hotmail.com>
2023-02-16 10:52:35 +0100

    tc8_doegnDosis

--- test/ordination/ordination/DagligSkaevTest.java
+++ test/ordination/ordination/DagligSkaevTest.java
@@ -2,11 +2,24 @@ package ordination.ordination;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligSkaevTest {
 
     @Test
-    void opretDosis() {
+    void t14_opretDosis() {
+        //arrange
+        Patient patient = new Patient("59841720", "Bingo Dorte", 78);
+        LocalTime[] kl = { LocalTime.of(12, 0), LocalTime.of(12, 40), LocalTime.of(16, 0), LocalTime.of(18, 45) };
+        double[] an = { 0.5, 1, 2.5, 3 };
+
+        //act
+        DagligSkaev dagligSkaev = new DagligSkaev(LocalDate.of(2022, 2, 3), LocalDate.of(2022, 2, 9), patient, kl, an);
+
+        //Assert
+        boolean actualBool = dagligSkaev.getDoser().contains(dagligSkaev);
     }
 }
