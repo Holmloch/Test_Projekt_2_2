53cb1cfab361a7a2e6d14e38676048513eae95a7
sodiee <mathi.carlsen@gmail.com>
2023-02-16 09:16:08 +0100

    h

--- test/ordination/controller/ControllerTest.java
+++ test/ordination/controller/ControllerTest.java
@@ -1,16 +1,27 @@
 package ordination.controller;
 
 
+import ordination.ordination.Laegemiddel;
+import ordination.ordination.Patient;
 import org.junit.jupiter.api.Test;
 
 class ControllerTest {
 
     @Test
-    void opretDagligFastOrdination() {
+    void tc6_opretDagligFastOrdination() {
+        //Arrange
+        Patient patient = new Patient("121256-0512", "Jane Jensen", 63.4);
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+
+        //Act
+
+        //Assert
+
     }
 
     @Test
     void anbefaletDosisPrDoegn() {
 
     }
+
 }
